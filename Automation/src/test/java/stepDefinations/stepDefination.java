2e837d505b5f2458ad79f9afac48d9d0e28d5be9
Manish7579 <manishsinghrajput2009@gmail.com>
2022-04-07 20:04:36 +0530

    first commit

---
+++ Automation/src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,41 @@
+package stepDefinations;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefination {
+
+	 @Given("^user is on landing page$")
+	    public void user_is_on_landing_page() throws Throwable {
+	        
+	        System.out.println("sucessfullay navigate to landing page");
+	    } 
+	 @When("^user login to application with \"([^\"]*)\" user and \"([^\"]*)\"$")
+	    public void user_login_to_application_with_something_user_and_something(String strArg1, String strArg2) throws Throwable {
+	       
+	    	  System.out.println(strArg1);
+	     	  System.out.println(strArg2);
+	    }
+
+	  @Then("^Home page is populated \"([^\"]*)\"$")
+	    public void home_page_is_populated_something(String strArg1) throws Throwable {
+	    	  System.out.println(strArg1);
+	    }
+
+	    @And("^some other check the radio buttom$")
+	    public void some_other_check_the_radio_buttom() throws Throwable {
+	    	  System.out.println("sucessfullay check the radio buttom");
+	    }
+
+	    @And("^yet another action$")
+	    public void yet_another_action() throws Throwable {
+	    	  System.out.println("sucessfullay scroll down the page");
+	    }
+
+	    @And("^check more outcomes$")
+	    public void check_more_outcomes() throws Throwable {
+	    	  System.out.println("sucessfullay automated the page");
+	    } 
+}
---
+++ Automation/target/test-classes/features/Login.feature
@@ -0,0 +1,43 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@tag
+Feature: Application Login
+  
+
+  @tag1
+  Scenario: Home Page Default Login
+    Given user is on landing page
+    When user login to application with "Manish" user and "manish7579"
+    And some other check the radio buttom
+    And yet another action
+    Then Home page is populated "true"
+    And check more outcomes
+    
+    
+   Scenario: Home Page Default Login
+    Given user is on landing page
+    When user login to application with "Rahul" user and "Rahul7579"
+    And some other check the radio buttom
+    And yet another action
+    Then Home page is populated "false"
+    And check more outcomes
+    
+    
+    
+
